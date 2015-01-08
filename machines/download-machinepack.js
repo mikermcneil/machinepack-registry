27e14247ef3c358962e401710b8145a11badeb9f
Mike McNeil <mike@balderdash.co>
2015-01-08 12:20:14 -0800

    Download machinepack.

--- machines/download-machinepack.js
+++ machines/download-machinepack.js
@@ -16,27 +16,28 @@ module.exports = {
     },
     success: {
       description: 'Done.',
-      example: {
-        identity: 'machinepack-whatever',
-        friendlyName: 'Whatever (you know)',
-        variableName: 'Whatever',
-        description: 'Node.js utilities for working with whatever.',
-        extendedDescription: '...',
-        npmPackageName: 'machinepack-whatever',
-        version: '1.2.3',
-        latestVersionPublishedAt: '2014-12-28T07:01:30.069Z',
-        nodeMachineUrl: 'http://node-machine.org/machinepack-whatever',
-        githubUrl: 'https://github.com/mikermcneil/machinepack-whatever',
-        npmUrl: 'http://npmjs.org/package/machinepack-whatever',
-        contributors: [{
-          name: 'Marty McFly',
-          email: 'marty@dolorian-tardis.edu'
-        }],
-        machines: [{
-          identity: 'do-stuff',
-          code: 'module.exports = {"identity": "do-stuff","friendlyName": "Do stuff and things","variableName": "doStuff","description": "Do stuff given other stuff.","fn":"thestringified fn"};'
-        }]
-      }
+      // example: {
+      //   identity: 'machinepack-whatever',
+      //   friendlyName: 'Whatever (you know)',
+      //   variableName: 'Whatever',
+      //   description: 'Node.js utilities for working with whatever.',
+      //   extendedDescription: '...',
+      //   npmPackageName: 'machinepack-whatever',
+      //   version: '1.2.3',
+      //   latestVersionPublishedAt: '2014-12-28T07:01:30.069Z',
+      //   nodeMachineUrl: 'http://node-machine.org/machinepack-whatever',
+      //   githubUrl: 'https://github.com/mikermcneil/machinepack-whatever',
+      //   npmUrl: 'http://npmjs.org/package/machinepack-whatever',
+      //   contributors: [{
+      //     name: 'Marty McFly',
+      //     email: 'marty@dolorian-tardis.edu'
+      //   }],
+      //   machines: [{
+      //     identity: 'do-stuff',
+      //     definition: {}
+      //     // code: 'module.exports = {"identity": "do-stuff","friendlyName": "Do stuff and things","variableName": "doStuff","description": "Do stuff given other stuff.","fn":"thestringified fn"};'
+      //   }]
+      // }
     }
   },
   fn: function(inputs, exits) {
@@ -100,18 +101,26 @@ module.exports = {
                   // Now load the JavaScript code string for each of this pack's machines.
                   var machinesAsJs = [];
                   async.each(machinepack.machines, function (machine, next){
-                    Machines.readMachineFile({
-                      source: path.resolve(machinesDirPath, machine.identity+'.js')
-                    }).exec({
-                      error: next,
-                      success: function (js){
-                        machinesAsJs.push({
-                          identity: machine.identity,
-                          code: js
-                        });
-                        next();
-                      }
+
+                    var machineDef = require(path.resolve(machinesDirPath, machine.identity+'.js'));
+                    machinesAsJs.push({
+                      identity: machine.identity,
+                      definition: stringifyRuntimeMachine(machineDef)
                     });
+                    next();
+
+                    // Machines.readMachineFile({
+                    //   source: path.resolve(machinesDirPath, machine.identity+'.js')
+                    // }).exec({
+                    //   error: next,
+                    //   success: function (js){
+                    //     machinesAsJs.push({
+                    //       identity: machine.identity,
+                    //       code: js
+                    //     });
+                    //     next();
+                    //   }
+                    // });
                   }, function afterwards(err){
                     if (err) return exits.error(err);
 
@@ -137,7 +146,7 @@ module.exports = {
       str += _.reduce(obj, function(memo, val, key) {
         var str = '"' + key + '": ';
         if (typeof val == 'object' && val !== null) {
-          str += MachineService.stringify(val);
+          str += stringifyRuntimeMachine(val);
         } else if (typeof val == 'function') {
           var fn = val.toString();
           fn = fn.replace(/^([\W]*?function\s+\(.*?\).*?\{[\W*])([\w\W]*)\}$/, "$2");
